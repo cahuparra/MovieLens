12100e15eb9fcf3a8781402093ff7fa2a7408aaa
cahuparra <cparra@dxc.com>
2021-12-09 16:41:24 -0600

    Finalizing testing

--- MoviLens.R
+++ MoviLens.R
@@ -58,6 +58,16 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 RMSE <- function(true_ratings, predicted_ratings){                                         
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
+
+#Use variable timestamp to generate predictor week
+edx<- edx %>% 
+  mutate(week = round_date(as_datetime(timestamp), unit = "week"))
+# Identify unique number of predictors (movie, user, time (weeks))
+edx %>% 
+  summarize(n_users = n_distinct(userId),
+              n_movies = n_distinct(movieId),
+            n_weeks = n_distinct(week))
+
 #Normalization of Effects using a model based approach
 # Equation: Y_ui=mu+bi+bu+b_t epsilon_ui
 #    Estimate mu (average rating of all movies accross all users)
@@ -66,7 +76,6 @@ mu<-mean(edx$rating)
 #   Regularize for movie, user, and effect
 #     tune lambda symmetrically for bi and bu and bt to minimize RMSE by using cross-validation
 lambdas <- seq(0, 5, 0.25)
-edx<- edx %>% mutate(week = round_date(as_datetime(timestamp), unit = "week"))
 rmses <- sapply(lambdas, function(l){
   b_i <- edx %>%
     group_by(movieId) %>%
@@ -75,12 +84,6 @@ rmses <- sapply(lambdas, function(l){
     left_join(b_i, by="movieId") %>%
     group_by(userId) %>%
     summarize(b_u = sum(rating - b_i - mu)/(n()+l))
-  
-  f_ui<-edx %>% 
-    left_join(b_u, by='userId') %>%
-    group_by(week) %>%
-    summarize(f_ui=log(n()/7))
-
   
   b_t <- edx %>%
     left_join(b_i, by='movieId') %>%
@@ -92,7 +95,7 @@ rmses <- sapply(lambdas, function(l){
     left_join(b_i, by = "movieId") %>%
     left_join(b_u, by = "userId") %>%
     left_join(b_t, by = "week") %>%
-    mutate(pred = mu + b_i + b_u + b_t + ) %>%
+    mutate(pred = mu + b_i + b_u + b_t) %>%
     .$pred
   return(RMSE(predicted_ratings, edx$rating))
 })
@@ -100,13 +103,14 @@ rmses <- sapply(lambdas, function(l){
 qplot(lambdas, rmses)  
 lambda <- lambdas[which.min(rmses)]
 min(rmses)    #0.8565926
-
-
-
+predicted_ratings
+nrow(b_t)
+nrow(f_ui)
+f_ui
 f_ui<-edx %>% 
   left_join(b_u, by='userId') %>%
   group_by(week) %>%
-  summarize(f_ui=int(log(n()/7)))
+  summarize(f_ui=log(n()/7))
   
   
 #Execute Regularization with optimized lambda
@@ -117,21 +121,118 @@ b_u <- edx %>%
   left_join(b_i, by="movieId") %>%
   group_by(userId) %>%
   summarize(b_u = sum(rating - b_i - mu)/(n()+lambda))
+b_t <- edx %>%
+  left_join(b_i, by='movieId') %>%
+  left_join(b_u, by='userId') %>%
+  group_by(week) %>%
+  summarize(b_t = sum(rating - mu - b_i - b_u)/(n()+lambda))
 predicted_ratings <- 
   edx %>% 
   left_join(b_i, by = "movieId") %>%
   left_join(b_u, by = "userId") %>%
-  mutate(pred = mu + b_i + b_u ) %>%
+  left_join(b_t, by = "week") %>%
+   mutate(pred = mu + b_i + b_u +b_t ) %>%
   .$pred
 
 model_3_rmse <- RMSE(predicted_ratings, edx$rating)
-model_3_rmse   #0.8566952
+model_3_rmse   #0.8565926
 
 #______________________
-s<-svd(y)
-s$d[1:50]
-dim(s$v)
+# Vn for users and Ui for movies
 
+
+freq_usr <- edx %>% group_by(week) %>%
+     select(week, userId,  rating) %>%
+     group_by(week,userId) %>% summarize(n=n(),avg_rat=mean(rating))
+dim(freq_usr)
+
+
+# DATA HISTOGRAMS
+# histogram movie effect b_i 
+
+edx %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = sum(rating - mu)) %>%       
+  ggplot(aes(b_i)) + 
+  geom_histogram(bins = 30, color = "blue")
+# histogram  user effect b_u
+edx %>% 
+  left_join(b_i, by="movieId") %>%
+  group_by(userId) %>%
+  summarize(b_u = sum(rating - b_i - mu)) %>%
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "black",fill="green")
+
+#histogram movie effect b_i with regularization
+edx %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = sum(rating - mu)/(n()+lambda)) %>%       
+  ggplot(aes(b_i)) + 
+  geom_histogram(bins = 30, color = "blue") 
+# histogram  user effect b_u with regularization
+edx %>% 
+  left_join(b_i, by="movieId") %>%
+  group_by(userId) %>%
+  summarize(b_u = sum(rating - b_i - mu)/(n()+lambda)) %>%
+    ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "black",fill="green")
+#histogram time effect b_t with regularization
+edx %>%
+  left_join(b_i, by='movieId') %>%
+  left_join(b_u, by='userId') %>%
+  group_by(week) %>%
+  summarize(b_t = sum(rating - mu - b_i - b_u)/(n()+lambda)) %>%
+  ggplot(aes(b_t)) + 
+  geom_histogram(bins = 30, color = "black",fill="blue")
+
+
+edx %>% 
+  left_join(b_i, by = "movieId") %>%
+  left_join(b_u, by = "userId") %>%
+  left_join(b_t, by = "week") %>%
+  mutate(pred = mu + b_i + b_u +b_t)%>%  
+  ggplot(aes(rating,pred)) + geom_point()
+  
+  geom_histogram(bins = 30, color = "black",fill="black")
+
+
+
+
+
+edx  %>% ggplot(aes(rating)) +geom_histogram(bins=30,fill="black")
+
+str(predicted_ratings)
+# histogram average rating for user u (user effect)
+edx %>% 
+  group_by(userId) %>% 
+  summarize(b_u = mean(rating)) %>% 
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, colour = "red") 
+
+#graphic representing the rating trend through weeks
+edx %>% group_by(week) %>%
+  summarize(rating = mean(rating)) %>%
+  ggplot(aes(week, rating)) +
+  geom_point() +
+  geom_smooth()
+
+#graphic representing the effect of the user rating frequency on the average rating
+freq_usr %>% group_by(userId) %>% filter(n()>50) %>%
+  ggplot(aes(log(n/7),avg_rat))+geom_point()+geom_smooth()
+
+freq_usr %>% group_by(userId) %>% filter(n()>1000) %>%
+  ggplot(aes(userId,avg_rat,size=log(n/7)))+geom_point()+geom_smooth()
+
+freq_usr
+max(freq_usr$n)
+
+f_y <- edx %>% 
+  group_by(week) %>%
+  summarize(f_y=n(), user=userId)
+
+
+nrow(f_y)
+f_y
 
 # generate a userId, Title matrix with ratings 
 # consider only movies with more than 527 ratings (90%) and users with more than 49 reviews (90%)
@@ -237,4 +338,99 @@ model_1_rmse <- RMSE(predicted_ratings, edx$rating)
 model_1_rmse    #0.9423
 
 
+s<-svd(y)
+s$d[1:50]
+dim(s$u)
+svdratings1<- s$u %*% s$d %*% s$v
+s$u
+svdratings<- svdratings1 %*% t(s$v)
+str(s)
+plot(s$u[,1], ylim = c(-0.25, 0.25))
 
+nrow(f_ui)
+b_i[2]
+f_ui[2]
+log(f_ui[2])
+
+b_i1 <- edx %>%
+  group_by(movieId)
+?log
+f_ui<-edx %>% 
+  left_join(b_u, by='userId') %>%
+  group_by(week) %>%
+  summarize(f_ui=log(n()/7))
+b_t
+
+#Execute Regularization with optimized lambda
+b_i <- edx %>%
+  group_by(movieId) %>%
+  summarize(b_i = (sum(rating - mu)/(n()+lambda)))
+b_u <- edx %>% 
+  left_join(b_i, by="movieId") %>%
+  group_by(userId) %>%
+  summarize(b_u = sum(rating - b_i - mu)/(n()+lambda))
+b_t <- edx %>%
+  left_join(b_i, by='movieId') %>%
+  left_join(b_u, by='userId') %>%
+  left_join(f_ui, by= 'week') %>%
+  group_by(week) %>%
+  summarize(b_t = sum(rating - mu - b_i -b_i*f_ui - b_u)/(n()+lambda))
+f_ui<-edx %>% 
+  left_join(b_u, by='userId') %>%
+  group_by(week) %>%
+  summarize(f_ui=log(n()/7))
+predicted_ratings <- 
+  edx %>% 
+  left_join(b_i, by = "movieId") %>%
+  left_join(b_u, by = "userId") %>%
+  left_join(b_t, by = "week") %>%
+  mutate(pred = mu + b_i + b_u +b_t ) %>%
+  .$pred
+
+model_3_rmse <- RMSE(predicted_ratings, edx$rating)
+model_3_rmse   #0.8565926
+
+
+edx_factorization <- edx %>% select(movieId,userId,rating)
+validation_factorization <- validation %>% select(movieId,userId,rating)
+
+edx_factorization <- as.matrix(edx_factorization)
+validation_factorization <- as.matrix(validation_factorization)
+write.table(edx_factorization , file = "trainingset.txt" , sep = " " , row.names = FALSE, col.names = FALSE)
+
+
+
+write.table(validation_factorization, file = "validationset.txt" , sep = " " , row.names = FALSE, col.names = FALSE)
+set.seed(1)
+install.packages("recosystem")
+library(recosystem)
+training_dataset <- data_file( "trainingset.txt")
+
+validation_dataset <- data_file( "validationset.txt")
+r = Reco()
+opts = r$tune(training_dataset, opts = list(dim = c(10, 20, 30), lrate = c(0.1, 0.2),
+                                            costp_l1 = 0, costq_l1 = 0,
+                                            nthread = 1, niter = 10))
+r$train(training_dataset, opts = c(opts$min, nthread = 1, niter = 20))
+
+
+pred_file = tempfile()
+
+r$predict(validation_dataset, out_file(pred_file)) 
+print(scan(pred_file, n = 10))
+scores_real <- read.table("validationset.txt", header = FALSE, sep = " ")$V3
+scores_pred <- scan(pred_file)
+rm(edx.copy, valid.copy)
+
+rmse_mf <- RMSE(scores_real,scores_pred)
+rmse_mf <- RMSE(edx$rating,scores_pred)
+str(training_dataset)
+
+rmse_mf  
+
+b_g <- split_edx %>%
+  left_join(b_i, by='movieId') %>%
+  left_join(b_u, by='userId') %>%
+  left_join(b_y, by = 'year') %>%
+  group_by(genres) %>%
+  summarize(b_g = sum(rating - mu - b_i - b_u - b_y)/(n()+lambda), n_g = n())
